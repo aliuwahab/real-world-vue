38ccf363911fb9294958fa4fb7adef185dfb8003
Aliu Wahab Gbeila <a.gbeila@temper.works>
2020-04-14 15:21:07 +0200

    Setup configurations

---
+++ src/components/EventCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>This is an event</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            key: value
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
